5d33af9bc05fb2e0a745faa377db289b1ee0b956
Evandro Gardolin <evandrogardolinn@gmail.com>
2024-12-17 15:17:40 -0300

    feat: versiona api exemplo em laravel

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\Gate;
+
+use App\Models\Post;
+
+class PostController extends Controller implements HasMiddleware
+{
+    public static function middleware()
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show'])
+        ];
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::with('user')->latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post = $request->user()->posts()->create($validated);
+
+        return [
+            'post' => $post,
+            'user' => $post->user
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return ['post' => $post, 'user' => $post->user];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        Gate::authorize('modify', $post);
+
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post->update($validated);
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        Gate::authorize('modify', $post);
+
+        $post->delete();
+
+        return ['message' => 'Post was deleted'];
+    }
+}
